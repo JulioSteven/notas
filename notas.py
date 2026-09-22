7142700a7531ef4223d48d656b4b72cd9313952d
JulioSteven <jstevensimeon@gmail.com>
2026-09-22 09:42:05 -0500

    Agregados calculos de promedio y notas

--- notas.py
+++ notas.py
@@ -1,5 +1,9 @@
 notas = [18, 20, 15, 12]
 
 # Bucle correcto en Python
-for n in notas:
-    print(n)
+for i, nota in enumerate(notas, start=1):
+    print(f"Nota {i}: {nota}")
+promedio = sum(notas) / len(notas)
+nota_alta = max(notas)
+nota_baja = min(notas)
+print (f"promedio: {promedio}\nnota mas alta: {nota_alta}\nnota mas baja: {nota_baja}")
